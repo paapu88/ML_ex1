445391e7c018d31381e31d52e7f1e74180cc6e6e
paapu68 <markus.kaukonen@iki.fi>
2017-02-17 20:35:32 +0200

    working cg (a=small!)

--- ex1.py
+++ ex1.py
@@ -2,6 +2,7 @@ from warmUpExercise import warmUpExercise
 import matplotlib.pyplot as plt
 import numpy as np
 from computeCost import computeCost
+from gradienDescent import gradientDescent
 
 
 # part 1 warm up
@@ -28,11 +29,12 @@ data = np.asmatrix([myones, x]).transpose()
 print(data.shape)
 print (data)
 
-theta = np.matrix([[0], [0]])  # initialize fitting parameters
+theta = np.zeros(shape=(2,1))  # initialize fitting parameters
+# theta = np.matrix([[0], [0]])
 
 # Some gradient descent settings
 iterations = 1500
-alpha = 0.01
+alpha = 0.0001
 
 x=np.asmatrix(x)
 y=np.asmatrix(y)
@@ -43,4 +45,4 @@ print(x.shape, y.shape, theta.shape, data.shape)
 print("cost: ",computeCost(data, y, theta) )
 
 # run gradient descent
-theta = gradientDescent(X, y, theta, alpha, iterations);
+theta = gradientDescent(data, y, theta, alpha, iterations);
--- gradienDescent.py
+++ gradienDescent.py
@@ -9,23 +9,27 @@ def gradientDescent(x, y, theta, alpha, num_iters):
     :param num_iters: number of cg iterations
     :return:
     """
-import numpy as np
-from computeCost import computeCost
+    import numpy as np
+    from computeCost import computeCost
 
 
-m = len(y) # number of training examples
-J_history = np.zeros([num_iters, 1])
+    m = len(y) # number of training examples
+    J_history = np.zeros([num_iters, 1])
+    print (theta.shape)
 
-for iter in range(len(num_iters)):
+    for iter in range(num_iters):
+        txmy = theta.T * x.T -y
+        d1 = - alpha/m * np.sum(txmy)
+        d2 = - alpha/m * np.sum(np.multiply(txmy, x[:,1].T))
+        print ("changes", d1, d2)
+        theta[0,0] = theta[0,0] + d1
+        # print(txmy.shape, x[:,1].T.shape)
+        # print(np.multiply(txmy, x[:,1].T))
+        #print(iter, np.sum(np.multiply(txmy, x[:,1].T)))
+        theta[1,0] = theta[1,0] + d2
 
+        # Save the cost
+        J_history[iter] = computeCost(x, y, theta);
+        print("iter, cost: ", iter, J_history[iter], theta)
 
-
-
-
-    # Save the cost
-
-    J_history[iter] = computeCost(x, y, theta);
-
-end
-
-end
+    return theta, J_history
